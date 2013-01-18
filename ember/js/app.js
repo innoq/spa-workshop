13b18a0a156f23a134b7705e5b63a3d76c167b43
Robert Glaser <mail@robert-glaser.de>
2013-01-18 13:51:37 +0100

    foo

--- ember/js/app.js
+++ ember/js/app.js
@@ -1,5 +1,13 @@
+// Deal with *.*
+jQuery.ajaxSetup({
+  headers: { "Accept": "application/json" }
+});
+
 var App = Ember.Application.create();
 
+DS.RESTAdapter.configure("plurals", {
+  "inventory": "inventory"
+});
 App.store = DS.Store.create({
     revision: 11,
     adapter: DS.RESTAdapter.create({
@@ -7,10 +15,18 @@ App.store = DS.Store.create({
     })
 });
 
+App.Inventory = DS.Model.extend({
+  items: DS.hasMany('App.Item', {embedded: true})
+});
+
+App.Item = DS.Model.extend({
+  inventory: DS.belongsTo('App.Inventory')
+});
+
 App.Person = DS.Model.extend({
 });
 
-App.Person.find(1);
+// App.Person.find(1);
 
 App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
