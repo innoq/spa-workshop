19b9606fbe2323b7f122467446edd95de9e80af3
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2013-01-18 10:57:01 +0100

    Added basic Test-Model

--- ember/js/app.js
+++ ember/js/app.js
@@ -1,5 +1,17 @@
 var App = Ember.Application.create();
 
+App.store = DS.Store.create({
+    revision: 11,
+    adapter: DS.RESTAdapter.create({
+        url: 'https://books.innoq.com'
+    })
+});
+
+App.Person = DS.Model.extend({
+});
+
+App.Person.find(1);
+
 App.ApplicationController = Ember.Controller.extend();
 App.ApplicationView = Ember.View.extend({
   templateName: 'application'
